e8e0723726fefabc5acb6613cc7c17e93ee6cfd8
AntonG0rbachev <jopa@>
2024-09-24 11:40:46 +0300

    creating activity for quiz

--- app/src/main/java/com/example/secondmobileapp/MainActivity.kt
+++ app/src/main/java/com/example/secondmobileapp/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.secondmobileapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private lateinit var btnTrue: Button
+    private lateinit var btnFalse: Button
+    private lateinit var nextButton: Button
+    private lateinit var prevButton: Button
+    private lateinit var questionTextView: TextView
+
+    private val quizViewModel: MainActivityViewModel by lazy {
+        val provider = ViewModelProvider(this)
+        provider.get(MainActivityViewModel::class.java)
     }
 }
