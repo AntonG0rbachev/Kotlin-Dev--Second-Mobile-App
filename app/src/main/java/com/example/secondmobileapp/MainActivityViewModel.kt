70b0abc78b7c59a3d278c8edc2656bc6ad34d7fb
AntonG0rbachev <jopa@>
2024-09-24 11:31:35 +0300

    complete model for questions views

--- app/src/main/java/com/example/secondmobileapp/MainActivityViewModel.kt
+++ app/src/main/java/com/example/secondmobileapp/MainActivityViewModel.kt
@@ -14,4 +14,18 @@ class MainActivityViewModel : ViewModel() {
         Question(R.string.seventhQuestion, false),
     )
 
+    private var currentIndex = 0
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
+
+    fun moveToPrev() {
+        currentIndex = (questionBank.size + currentIndex - 1) % questionBank.size
+    }
+
 }
